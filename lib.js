0e58023e051a18c343461d57b0ddd4c7e6ada2b6
unknown <anamulislamshamim117@gmail.com>
2023-01-07 05:34:19 +0600

    Perform the integration test with jest

--- lib.js
+++ lib.js
@@ -1,5 +1,41 @@
-function userInfo(userId, title){
-    return `${userId} & ${ title }`
+function userInfo(userId, title) {
+    return `${userId} & ${title}`
 };
 
-module.exports = userInfo;
+// validateInput
+const validateInput = function (value, required, isNumber) {
+    if (!value) {
+        return false;
+    };
+    if (required && value.toString().trim().length === 0) {
+        return false;
+    };
+    if (isNumber && isNaN(+value)) {
+        return false;
+    };
+    return true;
+};
+
+// generate final result
+const generateResult = function (userid, title) {
+    return `User ID: ${userid} created an article titled ${title}`;
+};
+
+// check and generate function
+const checkAndGenerate = function (userId, title, article) {
+    // check validation
+    if (
+        !validateInput(userId, true, true) ||
+        !validateInput(title, true, false) ||
+        !validateInput(article, true, false)
+    ) {
+        return false;
+    }
+
+    // generate output
+    const resultText = generateResult(userId, title);
+    return resultText;
+}
+
+
+module.exports = { userInfo, validateInput, checkAndGenerate };
--- lib.test.js
+++ lib.test.js
@@ -1,6 +1,16 @@
-const userInfo = require("./lib");
+const {userInfo, validateInput, checkAndGenerate} = require("./lib");
 
-
-test("testing generate result", () =>{
+// unit test for string generator function
+test("testing generate result function", () =>{
     expect(userInfo("117", "Shamim")).toBe("117 & Shamim");
-})
+});
+
+// unit test for validateInput
+test("testing validateInput funciton", () => {
+    expect(validateInput(1, true, true)).toBeTruthy();
+});
+
+// integration testing
+test("testing the checkandgenerate function", () => {
+    expect(checkAndGenerate(1,"Title 1", "This is a integration test")).toBe("User ID: 1 created an article titled Title 1")
+});
